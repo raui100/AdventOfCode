2582f0fcacde2454da0820001c2cfe33e051f34a
Raui Ghazaleh <rj.ghazaleh@gmail.com>
2020-12-04 21:26:17 +0100

    Implemented a generic solution for day 3

--- src/day03.py
+++ src/day03.py
@@ -1,13 +1,19 @@
+from collections import namedtuple
+from math import prod
+
 from lib.paths import data_directory
+
+
+def encounters(steps_right: int, steps_down: int):
+    """Determines encounters with the given parameters"""
+    position = namedtuple("Position", "x y")
+    positions = [position(x_pos, (steps_right * x_pos) % map_len) for x_pos in range(0, len(data), steps_down)]
+    return sum(map(lambda pos: data[pos.x][pos.y] == "#", positions))
 
 
 data = (data_directory / "day03.txt").read_text().split("\n")  # Splits the file at every newline
 data.pop()  # The last line of data is an empty string
+map_len = len(data[0])
 
-encounters: int = 0
-for index, row in enumerate(data):
-    y_position = (3 * index) % len(row)
-    if row[y_position] == "#":
-        encounters += 1
-
-print(encounters)
+lst_encounters = [encounters(right, down) for right, down in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]]
+print(prod(lst_encounters))
