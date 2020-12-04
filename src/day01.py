c4e29f1d009e0e70bdce131d88999126148d95e0
Raui Ghazaleh <rj.ghazaleh@gmail.com>
2020-12-04 18:56:01 +0100

    Simple solution to part 2 of day 1

--- src/day01.py
+++ src/day01.py
@@ -2,4 +2,12 @@ from lib.paths import data_directory
 
 
 years: list = [int(d) for d in (data_directory / "input").read_text().split()]  # Casts file to list of integer
-print(next((year * (2020 - year) for year in years if (2020 - year) in years)))  # Generator function
+year_threshold = 2020 - min(years)
+for year_1 in years:
+    for year_2 in years:
+        if year_1 + year_2 > year_threshold:  # Skipping year combinations that can't have a solution
+            continue
+
+        for year_3 in years:
+            if year_1 + year_2 + year_3 == 2020:
+                print(year_1 * year_2 * year_3)
