e46308f0cd227516f6da79054406142b7126c67f
Raui Ghazaleh <rj.ghazaleh@gmail.com>
2020-12-04 20:09:46 +0100

    Solved part two of day 2

--- src/day02.py
+++ src/day02.py
@@ -10,12 +10,12 @@ valid_password = 0
 for entry in data:
     match = regex.match(entry)
     with suppress(AttributeError):
-        minimum = int(match.group(1))
-        maximum = int(match.group(2))
-        validator = match.group(3)
+        index_1 = int(match.group(1)) - 1  # Cast one index to the glorious zero index
+        index_2 = int(match.group(2)) - 1  # Cast one index to the glorious zero index
+        character = match.group(3)
         password = match.group(4)
 
-        if minimum <= password.count(validator) <= maximum:
+        if sum((password[index_1] == character, password[index_2] == character)) == 1:
             valid_password += 1
 
 print(valid_password)
