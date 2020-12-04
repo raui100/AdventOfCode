0d1d4cd18cb8447eef425db46902eedd8ddc4b73
Raui Ghazaleh <rj.ghazaleh@gmail.com>
2020-12-04 19:34:21 +0100

    Implemented a generic solution for n entries. It is very slow though...

--- src/day01.py
+++ src/day01.py
@@ -1,13 +1,31 @@
+from itertools import product
+from math import prod
+
 from lib.paths import data_directory
 
 
-years: list = [int(d) for d in (data_directory / "input").read_text().split()]  # Casts file to list of integer
-year_threshold = 2020 - min(years)
-for year_1 in years:
-    for year_2 in years:
-        if year_1 + year_2 > year_threshold:  # Skipping year combinations that can't have a solution
-            continue
+def iter_list(number_of_entries: int) -> int:
+    """Tries to determine a result that sums 'number_of_entries' to 2020 and returns their product
 
-        for year_3 in years:
-            if year_1 + year_2 + year_3 == 2020:
-                print(year_1 * year_2 * year_3)
+    Args:
+        number_of_entries: Number of entries that should sum to 2020
+
+    Returns:
+        Product of the years that sum two 2020 if a solution is possible
+    Raises:
+        RuntimeError: There is no possible solution
+    """
+    for indices in product(*(iterable_years,) * number_of_entries):
+        if sum(valid_years := [years[index] for index in indices]) == 2020:
+
+            return prod(valid_years)
+
+    else:
+
+        raise RuntimeError(f"There is no possible solution for {number_of_entries} number of entries")
+
+
+years: list = [int(d) for d in (data_directory / "input").read_text().split()]  # Casts file to list of integer
+iterable_years = range(len(years) - 1)
+print(iter_list(2))
+print(iter_list(3))
