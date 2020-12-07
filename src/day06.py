41d2d29a9912cc7010750c659d5db1a3ddb648cf
Raui Ghazaleh <rj.ghazaleh@gmail.com>
2020-12-07 23:03:24 +0100

    Day 6 - Part 2

--- src/day06.py
+++ src/day06.py
@@ -1,8 +1,17 @@
+from typing import List, Set
+
 from lib.paths import get_day
 import re
 
 
-data = get_day(6).read_text().split("\n\n")  # Splits list at double newline
-data = [re.sub(r"[^a-z]", "", entry) for entry in data]  # Deletes every character that is not a-z
+def intersecting_entries(group: str) -> int:
+    """Counts the number of chars that are in every entry of the list"""
+    entries = group.splitlines()  # Splits the group into their members
+    entries: List[str] = [re.sub(r"[^a-z]", "", entry) for entry in entries]  # Deletes every char that is not a-z
+    entries: List[Set[str]] = [set(entry) for entry in entries]
 
-print(sum(map(lambda x: len(set(x)), data)))
+    return len(set.intersection(*tuple(entries)))
+
+
+data: List[str] = get_day(6).read_text().split("\n\n")  # Splits data into groups
+print(sum([intersecting_entries(group) for group in data]))
