56281bd15658ab5861899b8e7668069e549d3e57
Raui Ghazaleh <rj.ghazaleh@gmail.com>
2020-12-07 22:17:50 +0100

    Day 5 - Part 2

--- src/day05.py
+++ src/day05.py
@@ -1,15 +1,22 @@
+from typing import List
+
 from lib.paths import get_day
 import re
 
 
 def decode(seat: str) -> int:
-    """Decodes the binary representation the seat ID to a base 10 integer one"""
+    """Decodes the binary representation of the seat ID to a base 10 integer one"""
     seat = re.sub("[BR]", "1", seat)  # Replaces "B" or "R" with 1
     seat = re.sub("[FL]", "0", seat)  # Replaces "F" or "L" with 0
-    seat_id = int("0b" + seat, 2)  # Casts binary representation to integer base 10
+    seat = int("0b" + seat, 2)  # Casts binary representation to integer base 10
 
-    return seat_id
+    return seat
 
 
-data = get_day(5).read_text().splitlines()
-print(max(map(decode, data)))
+if __name__ == "__main__":
+    data: List[str] = get_day(5).read_text().splitlines()  # Reads the data as list of lines
+    data: List[int] = list(map(decode, data))  # Decodes the data to base 10 integer
+
+    for seat_id in range(1, max(data)):
+        if seat_id - 1 in data and seat_id not in data and seat_id + 1 in data:
+            print(seat_id)
