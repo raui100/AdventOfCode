a49da04dce61950eb9f6c3f3d9dc6dccbb94b4d4
Raui Jacob Ghazaleh <rj.ghazaleh@gmail.com>
2021-12-01 20:16:17 +0100

    Refactoring for Advent of Code 2021

--- 2020/python/lib/paths.py
+++ 2020/python/lib/paths.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-data_directory = Path(__file__).resolve().parent.parent / "data"
+data_directory = Path(__file__).resolve().parent.parent.parent / "input"
 
 
 def get_day(day: int) -> Path:
