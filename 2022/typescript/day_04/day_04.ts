e8e6e9cd4af3e630fcfdf107820a899de50cd007
Raui Ghazaleh <rj.ghazaleh@gmail.com>
2022-12-04 20:56:27 +0100

    Implemented day 4 in TypeScript

---
+++ 2022/typescript/day_04/day_04.ts
@@ -0,0 +1,7 @@
+const text: string = await Deno.readTextFile("04.txt", "utf8");
+
+let numbers: Array<Array<number>> = text.split("\n")
+    .map(line => line.replace(",", "-").split("-").map(Number))
+
+console.log(numbers.filter(nums => (nums[0] - nums[2]) * (nums[1] - nums[3]) <= 0).length)  // 547
+console.log(numbers.filter(nums => (nums[0] - nums[2]) * (nums[1] - nums[3]) >= 0).length)  // 784
