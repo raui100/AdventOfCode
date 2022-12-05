b89ffa839c876ad5bcf70a9743ed250aeed946c1
Raui Ghazaleh <rj.ghazaleh@gmail.com>
2022-12-05 13:09:46 +0100

    Added a template for the Rust solution

--- 2022/rust/src/common/solution.rs
+++ 2022/rust/src/common/solution.rs
@@ -4,15 +4,10 @@ pub trait Solution {
     fn part_b(&self) -> Option<String> { None }
 
     fn solution(&self) {
-        match (self.part_a(), self.part_b()) {
-            (None, None) => panic!("Has not been implemented: {}", self.name()),
-            (a, b) => {
-                println!("{}", self.name());
-                if let Some(a) = a { println!("{a}") }
-                if let Some(b) = b { println!("{b}") }
-                println!();
-            }
-        };
+        println!("{}", self.name());
+        if let Some(a) = self.part_a() { println!("{a}") }
+        if let Some(b) = self.part_b() { println!("{b}") }
+        println!()
     }
 }
 
